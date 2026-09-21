6e2e5b36d75c319cd1b1d4fd788fe9554d1733d0
Joana Vilaça Maia <pg58816@uminho.pt>
2026-09-21 11:38:51 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "Joana_Maia" 
+__credits__ = ["Joana_Maia"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Joana_Maia"
+__email__ = "pg58816@uminho.pt"
 
 
 
